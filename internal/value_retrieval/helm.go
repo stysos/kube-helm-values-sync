663223c79282fd6ff3192bf38e537db660f8abba
som <samtyson555@gmail.com>
2025-04-05 00:44:21 +0100

    chuck all in one func for a quick test

--- internal/value_retrieval/helm.go
+++ internal/value_retrieval/helm.go
@@ -4,14 +4,40 @@ import (
 	"helm.sh/helm/v3/pkg/action"
 
 	"fmt"
+	"log"
+	"os"
 
+	"helm.sh/helm/v3/pkg/cli"
 )
 
 type HelmRetriever struct {
 }
 
 func (h *HelmRetriever) GetValues(name string) {
-	getVals := action.NewGetValues()
+
+	settings := cli.New()
+
+	actionConfig := new(action.Configuration)
+	// You can pass an empty string instead of settings.Namespace() to list
+	// all namespaces
+	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+		log.Printf("%+v", err)
+		os.Exit(1)
+	}
+
+	client := action.NewList(actionConfig)
+	// Only list deployed
+	client.Deployed = true
+	results, err := client.Run()
+	if err != nil {
+		log.Printf("%+v", err)
+		os.Exit(1)
+	}
+
+	for _, rel := range results {
+		log.Printf("%+v", rel)
+	}
+	getVals := action.NewGetValues(actionConfig)
 
 	fmt.Println(getVals.Run(name))
 }
